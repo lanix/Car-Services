0e90474f92aff9a06405b2a9e9733605fe1a04d5
Filiberto Cota <filiberto.cota@gmail.com>
2011-08-21 22:54:16 -0700

    Refactored Date and KM validators  - Deleted instance variable @last_service to keep consistence of data if more tan 1 seasons are using this class.  - Changed if block to def block to validate if the DB is empty.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,15 +1,20 @@
 class Service < ActiveRecord::Base
-	@last_service = Service.last
-	if @last_service != nil
-		validates_numericality_of :km, :greater_than => @last_service.km
-		validate :validates_date
-	end
+
+	validate :validates_km, :if => :something_in_db?
+	validate :validates_date, :if => :something_in_db?
 	validates_presence_of :km, :oil
 	
+	def validates_km
+		validates_numericality_of :km, :greater_than => Service.last.km
+	end
 	
 	def validates_date
 		if self.date < Service.last.date
 			errors.add(:date, "must be greater than last service (" + Service.last.date.to_s + ")")
 		end
 	end
+	
+	def something_in_db?
+		Service.any?
+	end 
 end
